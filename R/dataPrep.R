4e666a3e0e8a62d41ab0613ceb630931b39482d5
joshuajcarson <joshuajcarson@gmail.com>
2017-09-10 13:39:03 -0500

    Added predictions
    Taken from
    http://trevorstephens.com/kaggle-titanic-tutorial/getting-started-with-r/

--- R/dataPrep.R
+++ R/dataPrep.R
@@ -1,4 +1,20 @@
 library(data.table)
+library(rattle)
+library(rpart)
+library(rpart.plot)
+library(RColorBrewer)
+library(randomForest)
+library(party)
+
+downloadLibraries <- function() {
+  install.packages('data.table')
+  install.packages("https://togaware.com/access/rattle_5.0.14.tar.gz", repos=NULL, type="source")
+  install.packages('rpart.plot')
+  install.packages('RColorBrewer')
+  install.packages('RGtk2')
+  install.packages('randomForest')
+  install.packages('party')
+}
 
 importData <- function() {
   trainingData <<- fread('data/train.csv')
@@ -8,17 +24,78 @@ importData <- function() {
 setupFactorsInData <- function() {
   trainingData$Sex <<- as.factor(trainingData$Sex)
   trainingData$Embarked <<- as.factor(trainingData$Embarked)
-  trainingData$Cabin <<- as.factor(trainingData$Cabin)
-  trainingData$Ticket <<- as.factor(trainingData$Ticket)
   trainingData$Survived <<- as.factor(trainingData$Survived)
-  trainingData$SibSp <<- as.factor(trainingData$SibSp)
-  trainingData$Parch <<- as.factor(trainingData$Parch)
   
   testData$Sex <<- as.factor(testData$Sex)
   testData$Embarked <<- as.factor(testData$Embarked)
-  testData$Cabin <<- as.factor(testData$Cabin)
-  testData$Ticket <<- as.factor(testData$Ticket)
-  testData$SibSp <<- as.factor(testData$SibSp)
-  testData$Parch <<- as.factor(testData$Parch)
 }
 
+getFit <- function(dataSet) {
+  rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, 
+        data=dataSet, 
+        method='class',
+        control=rpart.control(minsplit = 20))
+}
+
+getFitWithMoreFactors <- function(dataSet) {
+  rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID, 
+        data=dataSet, 
+        method='class',
+        control=rpart.control(minsplit = 20, cp=0.005))
+}
+
+combineDataSets <- function() {
+  rbind(trainingData, testData, fill=TRUE)
+}
+
+createTitles <- function(dataSet) {
+  dataSet$Title <- sapply(dataSet$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+  dataSet$Title <- sub(' ', '', dataSet$Title)
+  dataSet$Title[dataSet$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+  dataSet$Title[dataSet$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+  dataSet$Title[dataSet$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+  dataSet$Title <- as.factor(dataSet$Title)
+  dataSet
+}
+
+createSurnames <- function(dataset) {
+  dataset$Surname <- sapply(dataset$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+  dataset
+}
+
+createFamilySize <- function(dataSet) {
+  dataSet$FamilySize <- dataSet$SibSp + dataSet$Parch + 1
+  dataSet
+}
+
+createFamilyId <- function(dataSet) {
+  dataSet$FamilyID <- paste(as.character(dataSet$FamilySize), dataSet$Surname, sep="")
+  dataSet$FamilyID[dataSet$FamilySize <= 2] <- 'Small'
+  famIds <- data.frame(table(dataSet$FamilyID))
+  famIds <- famIds[famIds$Freq < 3,]
+  dataSet$FamilyID[dataSet$FamilyID %in% famIds$Var1] <- 'Small'
+  dataSet$FamilyID <- factor(dataSet$FamilyID)
+  
+  dataSet$FamilyID2 <- as.character(dataSet$FamilyID)
+  dataSet$FamilyID2[dataSet$FamilySize <= 3] <- 'Small'
+  dataSet$FamilyID2 <- factor(dataSet$FamilyID2)
+  
+  dataSet
+}
+
+cleanData <- function(dataSet) {
+  ageFit <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + FamilySize,
+                  data=dataSet[!is.na(dataSet$Age),],
+                  method='anova')
+  dataSet$Age[is.na(dataSet$Age)] <- predict(ageFit, dataSet[is.na(dataSet$Age),])
+  
+  dataSet$Embarked[dataSet$Embarked == ''] <- "S"
+  
+  dataSet$Fare[which(is.na(dataSet$Fare))] <- median(dataSet$Fare, na.rm=TRUE)
+  
+  dataSet
+}
+
+addFeatureColumns <- function(dataSet) {
+  cleanData(createFamilyId(createFamilySize(createSurnames(createTitles(dataSet)))))
+}
--- R/manager.R
+++ R/manager.R
@@ -2,3 +2,35 @@ source('R/dataPrep.R')
 
 importData()
 setupFactorsInData()
+
+combinedDataSets <- combineDataSets()
+combinedDataSets <- addFeatureColumns(combinedDataSets)
+trainingData <- combinedDataSets[1:891,]
+testData <- combinedDataSets[892:1309,]
+
+#rpart
+fit <- getFit(trainingData)
+fancyRpartPlot(fit)
+Prediction <- predict(fit, testData, type='class')
+
+#rpart with engineered factors
+fitWithMoreFactors <- getFitWithMoreFactors(trainingData)
+fancyRpartPlot(fitWithMoreFactors)
+Prediction <- predict(fitWithMoreFactors, testData, type='class')
+
+#random forst with engineered factors
+fitRandomForest <- randomForest(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID2,
+                               data=trainingData,
+                               importance=TRUE,
+                               ntree=2000)
+Prediction <- predict(fitRandomForest, testData)
+
+#conditional inference trees
+fitConditional <- cforest(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+                          data=trainingData,
+                          controls=cforest_unbiased(ntree=2000, mtry=3))
+Prediction <- predict(fitConditional, testData, OOB=TRUE, type='response')
+
+#Submission Area For Kaggle. Make sure to use the Prediction of the fit you want
+submit <- data.frame(PassengerId = testData$PassengerId, Survived = Prediction)
+write.csv(submit, file = "submission.csv", row.names = FALSE)
